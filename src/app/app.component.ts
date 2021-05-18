2304cec1de204e6ab1796ddcd6b70682f16f5522
MohammedAli1129 <61137464+MohammedAli1129@users.noreply.github.com>
2021-05-18 09:05:06 +0530

    fluid angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DiceRollerContainerRuntimeFactory } from "./fluid/containerCode";
+import { DiceRoller } from './fluid/dataObject';
+import { FluidLoaderService } from './fluid/fluid.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fluid-play';
+  createNew = false;
+  diceRoller: DiceRoller;
+  diceValue : string;
+  diceColor : string;
+  diceRolledSub : Subscription;
+
+  constructor(
+    private fluidService: FluidLoaderService, 
+    private changeDetector: ChangeDetectorRef
+  ) {}
+
+  async ngOnInit() {
+    this.diceRoller = await this.fluidService.loadDataObject<DiceRoller>(DiceRollerContainerRuntimeFactory);
+    this.diceRolledSub = this.diceRoller.diceRolled$.subscribe(this.updateDiceChar);
+  }
+
+  updateDiceChar = (val: number) => {
+    // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
+    this.diceValue = String.fromCodePoint(0x267F + val);
+    this.diceColor = `hsl(${val * 60}, 70%, 50%)`;
+    // diceRolled event is occuring outside of Angular so detecting changes
+    this.changeDetector.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.diceRolledSub.unsubscribe();
+  }
 }
